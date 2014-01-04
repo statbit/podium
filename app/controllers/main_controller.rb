05ba84e3cf91ea485ab7bdccc395ef8e24165a9f
John Brosnan <jbrosnan@constantcontact.com>
2014-01-04 16:39:01 -0500

    initial work

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,16 @@
+class MainController < ApplicationController
+  def show
+  end
+
+  def all_on_spotify
+  end
+
+  def most_on_spotify
+  end
+
+  def all_on_itunes
+  end
+
+  def most_on_itunes
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Podium::Application.config.secret_key_base = 'cd3f2101bf56d454c0dbd68afb23f19be8d0063e701b340d28a763540dd39f839c3644103070ecff957f0696325f8a7369258dad013e242d7fa0cb77c2808176'
